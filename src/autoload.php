c035d88be160af84495d1d2f7ee44b2b080a7d78
aVadim <vadim483@gmail.com>
2021-09-28 22:02:34 +0300

    fix autoload.php

--- src/autoload.php
+++ src/autoload.php
@@ -3,7 +3,7 @@
 spl_autoload_register(static function ($class) {
     $namespace = 'avadim\\FastExcelWriter\\';
     if (0 === strpos($class, $namespace)) {
-        include __DIR__ . '/FastExcelWriter/' . str_replace($namespace, '/', $class) . '.php';
+        include __DIR__ . '/FastExcelWriter/' . str_replace($namespace, '', $class) . '.php';
     }
 });
 

b125f8fa0515e1976a3a09069398d9d74d085891
aVadim <vadim483@gmail.com>
2023-06-14 00:46:42 +0300

    supports _xlnf functions

--- src/FastExcelWriter/Writer.php
+++ src/FastExcelWriter/Writer.php
@@ -12,6 +12,162 @@ use avadim\FastExcelWriter\Exception\ExceptionFile;
  */
 class Writer
 {
+    // These constants from phpoffice/phpspreadsheet ver.1.28
+    const XLFNREGEXP = '/(?:_xlfn\.)?((?:_xlws\.)?('
+    // functions added with Excel 2010
+    . 'beta[.]dist'
+    . '|beta[.]inv'
+    . '|binom[.]dist'
+    . '|binom[.]inv'
+    . '|ceiling[.]precise'
+    . '|chisq[.]dist'
+    . '|chisq[.]dist[.]rt'
+    . '|chisq[.]inv'
+    . '|chisq[.]inv[.]rt'
+    . '|chisq[.]test'
+    . '|confidence[.]norm'
+    . '|confidence[.]t'
+    . '|covariance[.]p'
+    . '|covariance[.]s'
+    . '|erf[.]precise'
+    . '|erfc[.]precise'
+    . '|expon[.]dist'
+    . '|f[.]dist'
+    . '|f[.]dist[.]rt'
+    . '|f[.]inv'
+    . '|f[.]inv[.]rt'
+    . '|f[.]test'
+    . '|floor[.]precise'
+    . '|gamma[.]dist'
+    . '|gamma[.]inv'
+    . '|gammaln[.]precise'
+    . '|lognorm[.]dist'
+    . '|lognorm[.]inv'
+    . '|mode[.]mult'
+    . '|mode[.]sngl'
+    . '|negbinom[.]dist'
+    . '|networkdays[.]intl'
+    . '|norm[.]dist'
+    . '|norm[.]inv'
+    . '|norm[.]s[.]dist'
+    . '|norm[.]s[.]inv'
+    . '|percentile[.]exc'
+    . '|percentile[.]inc'
+    . '|percentrank[.]exc'
+    . '|percentrank[.]inc'
+    . '|poisson[.]dist'
+    . '|quartile[.]exc'
+    . '|quartile[.]inc'
+    . '|rank[.]avg'
+    . '|rank[.]eq'
+    . '|stdev[.]p'
+    . '|stdev[.]s'
+    . '|t[.]dist'
+    . '|t[.]dist[.]2t'
+    . '|t[.]dist[.]rt'
+    . '|t[.]inv'
+    . '|t[.]inv[.]2t'
+    . '|t[.]test'
+    . '|var[.]p'
+    . '|var[.]s'
+    . '|weibull[.]dist'
+    . '|z[.]test'
+    // functions added with Excel 2013
+    . '|acot'
+    . '|acoth'
+    . '|arabic'
+    . '|averageifs'
+    . '|binom[.]dist[.]range'
+    . '|bitand'
+    . '|bitlshift'
+    . '|bitor'
+    . '|bitrshift'
+    . '|bitxor'
+    . '|ceiling[.]math'
+    . '|combina'
+    . '|cot'
+    . '|coth'
+    . '|csc'
+    . '|csch'
+    . '|days'
+    . '|dbcs'
+    . '|decimal'
+    . '|encodeurl'
+    . '|filterxml'
+    . '|floor[.]math'
+    . '|formulatext'
+    . '|gamma'
+    . '|gauss'
+    . '|ifna'
+    . '|imcosh'
+    . '|imcot'
+    . '|imcsc'
+    . '|imcsch'
+    . '|imsec'
+    . '|imsech'
+    . '|imsinh'
+    . '|imtan'
+    . '|isformula'
+    . '|iso[.]ceiling'
+    . '|isoweeknum'
+    . '|munit'
+    . '|numbervalue'
+    . '|pduration'
+    . '|permutationa'
+    . '|phi'
+    . '|rri'
+    . '|sec'
+    . '|sech'
+    . '|sheet'
+    . '|sheets'
+    . '|skew[.]p'
+    . '|unichar'
+    . '|unicode'
+    . '|webservice'
+    . '|xor'
+    // functions added with Excel 2016
+    . '|forecast[.]et2'
+    . '|forecast[.]ets[.]confint'
+    . '|forecast[.]ets[.]seasonality'
+    . '|forecast[.]ets[.]stat'
+    . '|forecast[.]linear'
+    . '|switch'
+    // functions added with Excel 2019
+    . '|concat'
+    . '|countifs'
+    . '|ifs'
+    . '|maxifs'
+    . '|minifs'
+    . '|sumifs'
+    . '|textjoin'
+    // functions added with Excel 365
+    . '|filter'
+    . '|randarray'
+    . '|anchorarray'
+    . '|sequence'
+    . '|sort'
+    . '|sortby'
+    . '|unique'
+    . '|xlookup'
+    . '|xmatch'
+    . '|arraytotext'
+    . '|call'
+    . '|let'
+    . '|lambda'
+    . '|single'
+    . '|register[.]id'
+    . '|textafter'
+    . '|textbefore'
+    . '|textsplit'
+    . '|valuetotext'
+    . '))\s*\(/Umui';
+
+    const XLWSREGEXP = '/(?<!_xlws\.)('
+    // functions added with Excel 365
+    . 'filter'
+    . '|sort'
+    . ')\s*\(/mui';
+
     /** @var Excel */
     protected $excel;
 
@@ -418,6 +574,8 @@ class Writer
     {
         static $functionNames = [];
 
+        $formula = substr($formula, 1);
+
         $mark = md5(microtime());
         $replace = [];
         // temporary replace strings
@@ -461,6 +619,13 @@ class Writer
             $formula = str_replace($replace[0], $replace[1], $formula);
         }
 
+        if ($formula) {
+            $formula = (string) preg_replace(self::XLFNREGEXP, '_xlfn.$1(', $formula);
+        }
+        if ($formula) {
+            $formula = (string) preg_replace(self::XLWSREGEXP, '_xlws.$1(', $formula);
+        }
+
         return $formula;
     }
 
@@ -484,6 +649,7 @@ class Writer
         }
         elseif (is_string($value) && $value[0] === '=') {
             // formula
+            //$value = '_xlfn.' . $this->_convertFormula(substr($value, 1), [$rowNumber, $colNumber]);
             $value = $this->_convertFormula($value, [$rowNumber, $colNumber]);
             $file->write('<c r="' . $cellName . '" s="' . $cellStyleIdx . '" t="s"><f>' . self::xmlSpecialChars($value) . '</f></c>');
         }

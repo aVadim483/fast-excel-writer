9ee5fc9e1191ea15a11ec0877b76ef3e67ffd8e9
aVadim <vadim483@gmail.com>
2025-01-18 14:58:40 +0300

    upd: class Style

--- src/FastExcelWriter/Style.php
+++ src/FastExcelWriter/Style.php
@@ -6,63 +6,65 @@ class Style
 {
     public const FORMAT = 'format';
 
-    public const FONT               = 'font';
-    public const FONT_NAME          = 'font-name';
-    public const FONT_STYLE         = 'font-style';
-    public const FONT_STYLE_BOLD    = 'font-style-bold';
-    public const FONT_STYLE_ITALIC  = 'font-style-italic';
-    public const FONT_STYLE_UNDERLINE  = 'font-style-underline';
-    public const FONT_STYLE_STRIKETHROUGH  = 'font-style-strikethrough';
+    public const FONT = 'font';
+    public const FONT_NAME = 'font-name';
+    public const FONT_STYLE = 'font-style';
+    public const FONT_STYLE_BOLD = 'font-style-bold';
+    public const FONT_STYLE_ITALIC = 'font-style-italic';
+    public const FONT_STYLE_UNDERLINE = 'font-style-underline';
+    public const FONT_STYLE_STRIKE = 'font-style-strike';
+    public const FONT_STYLE_STRIKETHROUGH = 'font-style-strike';
 
-    public const FONT_SIZE          = 'font-size';
-    public const FONT_COLOR          = 'font-color';
+    public const FONT_SIZE = 'font-size';
+    public const FONT_COLOR = 'font-color';
 
-    public const STYLE              = 'style';
-    public const WIDTH              = 'width';
+    public const STYLE = 'style';
+    public const WIDTH = 'width';
 
-    public const TEXT_WRAP          = 'format-text-wrap';
-    public const TEXT_ALIGN         = 'format-align-horizontal';
-    public const VERTICAL_ALIGN     = 'format-align-vertical';
+    public const TEXT_WRAP = 'format-text-wrap';
+    public const TEXT_ALIGN = 'format-align-horizontal';
+    public const VERTICAL_ALIGN = 'format-align-vertical';
 
-    public const TEXT_ALIGN_LEFT    = 'left';
-    public const TEXT_ALIGN_CENTER  = 'center';
-    public const TEXT_ALIGN_RIGHT   = 'right';
+    public const TEXT_ALIGN_LEFT = 'left';
+    public const TEXT_ALIGN_CENTER = 'center';
+    public const TEXT_ALIGN_RIGHT = 'right';
 
-    public const FILL_COLOR         = 'fill-color';
+    public const FILL_COLOR = 'fill-color';
 
-    const FILL_SOLID                        = 'solid';
-    const FILL_GRADIENT                     = 'linear';
-    const FILL_GRADIENT_LINEAR              = 'linear';
+    const FILL_SOLID = 'solid';
+    const FILL_GRADIENT = 'linear';
+    const FILL_GRADIENT_LINEAR = 'linear';
 
-    public const FILL_PATTERN_DARKDOWN             = 'darkDown';
-    public const FILL_PATTERN_DARKGRAY             = 'darkGray';
-    public const FILL_PATTERN_DARKGRID             = 'darkGrid';
-    public const FILL_PATTERN_DARKHORIZONTAL       = 'darkHorizontal';
-    public const FILL_PATTERN_DARKTRELLIS          = 'darkTrellis';
-    public const FILL_PATTERN_DARKUP               = 'darkUp';
-    public const FILL_PATTERN_DARKVERTICAL         = 'darkVertical';
-    public const FILL_PATTERN_GRAY0625             = 'gray0625';
-    public const FILL_PATTERN_GRAY125              = 'gray125';
-    public const FILL_PATTERN_LIGHTDOWN            = 'lightDown';
-    public const FILL_PATTERN_LIGHTGRAY            = 'lightGray';
-    public const FILL_PATTERN_LIGHTGRID            = 'lightGrid';
-    public const FILL_PATTERN_LIGHTHORIZONTAL      = 'lightHorizontal';
-    public const FILL_PATTERN_LIGHTTRELLIS         = 'lightTrellis';
-    public const FILL_PATTERN_LIGHTUP              = 'lightUp';
-    public const FILL_PATTERN_LIGHTVERTICAL        = 'lightVertical';
-    public const FILL_PATTERN_MEDIUMGRAY           = 'mediumGray';
+    public const FILL_PATTERN = 'fill-pattern';
+    public const FILL_PATTERN_DARKDOWN = 'darkDown';
+    public const FILL_PATTERN_DARKGRAY = 'darkGray';
+    public const FILL_PATTERN_DARKGRID = 'darkGrid';
+    public const FILL_PATTERN_DARKHORIZONTAL = 'darkHorizontal';
+    public const FILL_PATTERN_DARKTRELLIS = 'darkTrellis';
+    public const FILL_PATTERN_DARKUP = 'darkUp';
+    public const FILL_PATTERN_DARKVERTICAL = 'darkVertical';
+    public const FILL_PATTERN_GRAY0625 = 'gray0625';
+    public const FILL_PATTERN_GRAY125 = 'gray125';
+    public const FILL_PATTERN_LIGHTDOWN = 'lightDown';
+    public const FILL_PATTERN_LIGHTGRAY = 'lightGray';
+    public const FILL_PATTERN_LIGHTGRID = 'lightGrid';
+    public const FILL_PATTERN_LIGHTHORIZONTAL = 'lightHorizontal';
+    public const FILL_PATTERN_LIGHTTRELLIS = 'lightTrellis';
+    public const FILL_PATTERN_LIGHTUP = 'lightUp';
+    public const FILL_PATTERN_LIGHTVERTICAL = 'lightVertical';
+    public const FILL_PATTERN_MEDIUMGRAY = 'mediumGray';
 
-    public const BORDER             = 'border';
+    public const BORDER = 'border';
 
-    public const BORDER_SIDE        = 1;
-    public const BORDER_STYLE       = 'style';
-    public const BORDER_COLOR       = 'color';
+    public const BORDER_SIDE = 1;
+    public const BORDER_STYLE = 'style';
+    public const BORDER_COLOR = 'color';
 
-    public const BORDER_LEFT        = 1;
-    public const BORDER_RIGHT       = 2;
-    public const BORDER_TOP         = 4;
-    public const BORDER_BOTTOM      = 8;
-    public const BORDER_ALL         = self::BORDER_TOP + self::BORDER_RIGHT + self::BORDER_BOTTOM + self::BORDER_LEFT;
+    public const BORDER_LEFT = 1;
+    public const BORDER_RIGHT = 2;
+    public const BORDER_TOP = 4;
+    public const BORDER_BOTTOM = 8;
+    public const BORDER_ALL = self::BORDER_TOP + self::BORDER_RIGHT + self::BORDER_BOTTOM + self::BORDER_LEFT;
 
     public const BORDER_NONE = null;
     public const BORDER_THIN = 'thin';
@@ -96,4 +98,447 @@ class Style
 
     public const BORDER_STYLE_MIN = self::BORDER_NONE;
     public const BORDER_STYLE_MAX = self::BORDER_SLANT_DASH_DOT;
+
+
+    protected array $styles = [];
+    
+    
+    protected function _setStyleOptions(string $primaryKey, array $options)
+    {
+        $this->styles[$primaryKey] = $options;
+    }
+    
+    
+    /**
+     * Sets all borders style
+     *
+     * @param string $style
+     * @param string|null $color
+     *
+     * @return $this
+     */
+    public function setBorder(string $style, ?string $color = '#000000'): Style
+    {
+        $options = [
+            'border-left-style' => $style,
+            'border-left-color' => $color,
+            'border-right-style' => $style,
+            'border-right-color' => $color,
+            'border-top-style' => $style,
+            'border-top-color' => $color,
+            'border-bottom-style' => $style,
+            'border-bottom-color' => $color,
+            'border-diagonal-up' => 0,
+            'border-diagonal-down' => 0,
+        ];
+
+        $this->_setStyleOptions(Style::BORDER, $options, true);
+
+        return $this;
+    }
+
+    /**
+     * @param string $style
+     * @param string|null $color
+     *
+     * @return $this
+     */
+    public function setBorderLeft(string $style, ?string $color = '#000000'): Style
+    {
+        $options = [
+            'border-left-style' => $style,
+            'border-left-color' => $color,
+        ];
+
+        $this->_setStyleOptions(Style::BORDER, $options);
+
+        return $this;
+    }
+
+    /**
+     * @param string $style
+     * @param string|null $color
+     *
+     * @return $this
+     */
+    public function setBorderRight(string $style, ?string $color = '#000000'): Style
+    {
+        $options = [
+            'border-right-style' => $style,
+            'border-right-color' => $color,
+        ];
+
+        $this->_setStyleOptions(Style::BORDER, $options);
+
+        return $this;
+    }
+
+    /**
+     * @param string $style
+     * @param string|null $color
+     *
+     * @return $this
+     */
+    public function setBorderTop(string $style, ?string $color = '#000000'): Style
+    {
+        $options = [
+            'border-top-style' => $style,
+            'border-top-color' => $color,
+        ];
+
+        $this->_setStyleOptions(Style::BORDER, $options);
+
+        return $this;
+    }
+
+    /**
+     * @param string $style
+     * @param string|null $color
+     *
+     * @return $this
+     */
+    public function setBorderBottom(string $style, ?string $color = '#000000'): Style
+    {
+        $options = [
+            'border-bottom-style' => $style,
+            'border-bottom-color' => $color,
+        ];
+
+        $this->_setStyleOptions(Style::BORDER, $options);
+
+        return $this;
+    }
+
+    /**
+     * @param string $fontName
+     * @param int|null $fontSize
+     * @param string|null $fontStyle
+     * @param string|null $fontColor
+     *
+     * @return $this
+     */
+    public function setFont(string $fontName, ?int $fontSize = null, ?string $fontStyle = null, ?string $fontColor = null): Style
+    {
+        $font = ['font-name' => $fontName];
+        if ($fontSize) {
+            $font['font-size'] = $fontSize;
+        }
+        if ($fontStyle) {
+            $font['font-style'] = $fontStyle;
+        }
+        if ($fontColor) {
+            $font['font-color'] = $fontColor;
+        }
+
+        $this->_setStyleOptions('font', $font);
+
+        return $this;
+    }
+
+
+    /**
+     * @param string $fontName
+     *
+     * @return $this
+     */
+    public function setFontName(string $fontName): Style
+    {
+        $this->_setStyleOptions('font', [Style::FONT_NAME => $fontName]);
+
+        return $this;
+    }
+
+    /**
+     * @param float $fontSize
+     *
+     * @return $this
+     */
+    public function setFontSize(float $fontSize): Style
+    {
+        $this->_setStyleOptions('font', [Style::FONT_SIZE => $fontSize]);
+
+        return $this;
+    }
+
+    /**
+     * @param string $fontStyle
+     *
+     * @return $this
+     */
+    public function setFontStyle(string $fontStyle): Style
+    {
+        $fontStyle = str_replace(['font-style-', 'font-'] , '', strtolower($fontStyle));
+        $this->_setStyleOptions('font', ['font-style-' . $fontStyle => 1]);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function setFontStyleBold(): Style
+    {
+        return $this->setFontStyle('bold');
+    }
+
+    /**
+     * @return $this
+     */
+    public function setFontStyleItalic(): Style
+    {
+        return $this->setFontStyle('italic');
+    }
+
+    /**
+     * @param bool|null $double
+     *
+     * @return $this
+     */
+    public function setFontStyleUnderline(?bool $double = false): Style
+    {
+        $this->_setStyleOptions('font', [Style::FONT_STYLE_UNDERLINE => $double ? 2 : 1]);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function setFontStyleStrikethrough(): Style
+    {
+        return $this->setFontStyle(Style::FONT_STYLE_STRIKETHROUGH);
+    }
+
+    /**
+     * @param string $fontColor
+     *
+     * @return $this
+     */
+    public function setFontColor(string $fontColor): Style
+    {
+        $this->_setStyleOptions('font', [Style::FONT_COLOR => $fontColor]);
+
+        return $this;
+    }
+
+    /**
+     * Alias of 'setFontColor()'
+     *
+     * @param string $color
+     *
+     * @return $this
+     */
+    public function setColor(string $color): Style
+    {
+        return $this->setFontColor($color);
+    }
+
+    /**
+     * Fill background color
+     *
+     * @param string $color
+     * @param string|null $pattern
+     *
+     * @return $this
+     */
+    public function setFillColor(string $color, ?string $pattern = null): Style
+    {
+        $this->_setStyleOptions('fill', [Style::FILL_COLOR => $color, Style::FILL_PATTERN => $pattern ?: 'solid']);
+
+        return $this;
+    }
+
+    /**
+     * Alias of 'setFillColor()'
+     *
+     * @param string $color
+     * @param string|null $pattern
+     *
+     * @return $this
+     */
+    public function setBgColor(string $color, ?string $pattern = null): Style
+    {
+        return $this->setFillColor($color, $pattern);
+    }
+
+    /**
+     * Fill background by gradient
+     *
+     * @param string $color1
+     * @param string $color2
+     * @param int|null $degree
+     *
+     * @return $this
+     */
+    public function setFillGradient(string $color1, string $color2, ?int $degree = null): Style
+    {
+        $this->_setStyleOptions('fill', [
+            'fill-pattern' => Style::FILL_GRADIENT_LINEAR,
+            'fill-gradient-start' => $color1,
+            'fill-gradient-end' => $color2,
+            'fill-gradient-degree' => $degree ?: 0,
+        ]);
+
+        return $this;
+    }
+
+    /**
+     * @param string $textAlign
+     * @param string|null $verticalAlign
+     *
+     * @return $this
+     */
+    public function setTextAlign(string $textAlign, ?string $verticalAlign = null): Style
+    {
+        $options = ['format-align-horizontal' => $textAlign];
+        if ($verticalAlign !== null) {
+            $options['format-align-vertical'] = $verticalAlign;
+        }
+        $this->_setStyleOptions('format', $options);
+
+        return $this;
+    }
+
+    /**
+     * @param string $verticalAlign
+     *
+     * @return $this
+     */
+    public function setVerticalAlign(string $verticalAlign): Style
+    {
+        $this->_setStyleOptions('format', ['vertical-align' => $verticalAlign]);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function setTextCenter(): Style
+    {
+        $this->_setStyleOptions('format', [Style::TEXT_ALIGN => 'center', Style::VERTICAL_ALIGN => 'center']);
+
+        return $this;
+    }
+
+    /**
+     * Apply left alignment to content
+     *
+     * @return $this
+     */
+    public function setAlignLeft(): Style
+    {
+        return $this->setTextAlign('left');
+    }
+
+    /**
+     * Apply right alignment to content
+     *
+     * @return $this
+     */
+    public function setAlignRight(): Style
+    {
+        return $this->setTextAlign('right');
+    }
+
+    /**
+     * @param bool|null $textWrap
+     *
+     * @return $this
+     */
+    public function setTextWrap(?bool $textWrap = true): Style
+    {
+        $this->_setStyleOptions('format', [Style::TEXT_WRAP => (int)$textWrap]);
+
+        return $this;
+    }
+
+    /**
+     * @param string $color
+     *
+     * @return $this
+     */
+    public function setTextColor(string $color): Style
+    {
+        $this->_setStyleOptions('font', [Style::FONT_COLOR => $color]);
+
+        return $this;
+    }
+
+    /**
+     * @param int $degrees
+     *
+     * @return $this
+     */
+    public function setTextRotation(int $degrees): Style
+    {
+        $this->_setStyleOptions('format', [ 'format-text-rotation' => $degrees ] );
+
+        return $this;
+    }
+
+    /**
+     * @param int $indent
+     *
+     * @return $this
+     */
+    public function setIndentLeft(int $indent): Style
+    {
+        $options = ['format-align-horizontal' => 'left', 'format-align-indent' => $indent];
+        $this->_setStyleOptions('format', $options);
+
+        return $this;
+    }
+
+    /**
+     * @param int $indent
+     *
+     * @return $this
+     */
+    public function setIndentRight(int $indent): Style
+    {
+        $options = ['format-align-horizontal' => 'right', 'format-align-indent' => $indent];
+        $this->_setStyleOptions('format', $options);
+
+        return $this;
+    }
+
+    /**
+     * @param int $indent
+     *
+     * @return $this
+     */
+    public function setIndentDistributed(int $indent): Style
+    {
+        $options = ['format-align-horizontal' => 'distributed', 'format-align-indent' => $indent];
+        $this->_setStyleOptions('format', $options);
+
+        return $this;
+    }
+
+    /**
+     * @param string|array $format
+     *
+     * @return $this
+     */
+    public function setFormat($format): Style
+    {
+        if (is_array($format)) {
+            $this->_setStyleOptions('format', $format);
+        }
+        else {
+            if ($format && $format[0] === '@') {
+                $format = strtoupper($format);
+            }
+            $this->_setStyleOptions('format', ['format-pattern' => $format]);
+        }
+
+        return $this;
+    }
+
+
+    public function toArray(): array
+    {
+        return $this->styles;
+    }
 }

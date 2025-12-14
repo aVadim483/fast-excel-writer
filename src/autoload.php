68fad67fbeb5b1649a8f245adfaeaef74eb4a0e8
aVadim <aVadim483@gmail.com>
2025-12-14 21:19:23 +0300

    upd: autoload.php

--- src/autoload.php
+++ src/autoload.php
@@ -3,10 +3,8 @@
 spl_autoload_register(static function ($class) {
     $namespace = 'avadim\\FastExcelWriter\\';
     if (0 === strpos($class, $namespace)) {
-        $file = str_replace('\\', '/', __DIR__ . '/FastExcelWriter/' . str_replace($namespace, '', $class) . '.php');
-        if (file_exists($file)) {
-            include $file;
-        }
+        $file = str_replace('\\', DIRECTORY_SEPARATOR, str_replace($namespace, '', $class) . '.php');
+        include_once __DIR__ . DIRECTORY_SEPARATOR . 'FastExcelWriter' . DIRECTORY_SEPARATOR . $file;
     }
 });
 
